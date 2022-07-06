5ca67514a9d39e7aef87a35a20c211e2cb0f104d
wishallmaurya <vm7740014@gmail.com>
2022-07-06 13:04:07 +0530

    usercontroller and user login almost done

---
+++ src/controller/userController.js
@@ -0,0 +1,94 @@
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken")
+const { isValidEmail, isValidName, isValidPhone, isValid, isValidPassword,isValidPincode } = require("../validation/validator");
+
+
+const createUser = async function (req, res) {
+    try {
+        let data = req.body;
+        const { title } = req.body
+
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({ msg: "Body should not be empty" })
+        }
+
+        if (!("title" in data) || !("name" in data) || !("phone" in data) || !("email" in data) || !("password" in data) || !("address" in data)) return res.status(400).send({ status: false, msg: "title,name,phone,email,password and address all are required" })
+
+        if (!isValid(data.title)) return res.status(400).send({ status: false, msg: "The Title Attributes should not be empty" })
+        if (title !== "Mr") {
+            if (title !== "Mrs") {
+                if (title !== "Miss") {
+                    return res.status(400).send({ status: false, msg: "Title Should be Mr , Mrs , Miss" })
+                }
+            }
+        }
+
+        if (!isValid(data.name)) return res.status(400).send({ status: false, msg: "The name Attributes should not be empty" })
+        if (!isValidName(data.name)) return res.status(400).send({ status: false, msg: "Pls Enter Valid Name" })
+
+        if (!isValid(data.phone))
+            return res.status(400).send({ status: false, msg: "The phone Attributes should not be empty" })
+        if (!isValidPhone(data.phone))
+            return res.status(400).send({ status: false, msg: "Pls Enter Valid phone" })
+
+        let checkuniquephone = await userModel.findOne({ phone: data.phone })
+        if (checkuniquephone) return res.status(400).send({ status: false, msg: "This phone Already Exists Pls Use Another" })
+
+        if (!isValid(data.email)) return res.status(400).send({ status: false, msg: "The email Attributes should not be empty" })
+        if (!isValidEmail(data.email)) return res.status(400).send({ status: false, msg: "Pls Enter Email in valid Format" })
+
+        let checkuniqueemail = await userModel.findOne({ email: data.email })
+        if (checkuniqueemail) return res.status(400).send({ status: false, msg: "This Email Id Already Exists Pls Use Another" })
+
+        if (!isValid(data.password)) return res.status(400).send({ status: false, msg: "The Password Attributes should not be empty" })
+        if (!isValidPassword(data.password)) return res.status(400).send({ status: false, msg: "Password is not valid- your password should be 8 to 15 digit long" })
+
+        if (!isValid(data.address.street)) return res.status(400).send({ status: false, msg: "The Password Attributes should not be empty" })
+        
+        if (!isValid(data.address.city)) return res.status(400).send({ status: false, msg: "The Password Attributes should not be empty" })
+        if (!isValidName(data.address.city)) return res.status(400).send({ status: false, msg: "Pls Enter Valid city Name" })
+
+        if (!isValid(data.address.pincode)) return res.status(400).send({ status: false, msg: "The Password Attributes should not be empty" })
+        if (!isValidPincode(data.address.pincode)) return res.status(400).send({ status: false, msg: "Pls Enter Valid Pincode" })
+
+        
+
+
+
+
+
+
+        let savedData = await userModel.create(data);
+        res.status(201).send({ status: true, msg: "user Created Successfully", data: savedData });
+    }
+
+    catch (error) {
+        return res.status(500).send({ status: false, msg: error.message })
+    }
+};
+
+//........................................................ User Login 
+const userLogin = async function (req, res) {
+    let value = req.body
+    let userName = value.email
+    let password = value.password
+    //................................................... Empty Body Validation 
+    if (!("email" in value) || !("password" in value)) return res.status(400).send({ status: false, msg: "Pls Enter Email And Password" })
+
+    //....................................................Empty Attributes Validation
+    if (!isValid(userName) || !isValid(password)) return res.status(400).send({ status: false, msg: "Pls Provide data in Email And Password" })
+
+    let user = await userModel.findOne({ $and: [{ email: userName, password: password }] })
+    if (!user) return res.status(404).send({ status: false, msg: "Pls Use Valid Credentials" })
+
+    let token = jwt.sign({
+        userId: user._id.toString()
+    }, "functionup-radon")
+
+    res.status(200).send({ status: true, token: token, msg: "Succesfully Login" })
+
+}
+
+
+module.exports.userLogin = userLogin
+module.exports.createUser = createUser
---
+++ src/validation/validator.js
@@ -0,0 +1,39 @@
+const isValidEmail=(mail)=>{
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+    return true
+}
+const isValidName=(name)=>{
+    if(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(name))
+    return true
+}
+const isValid=(value)=>{
+    if(typeof value==="undefined" || value=== null ) return false
+    if(typeof value==="string" && value.trim().length===0) return false
+    return true
+}
+const isValidPhone = (mobile) => {
+    if (/^([+]\d{2})?\d{10}$/.test(mobile))
+        return true
+}
+const isValidPincode = (pin) => {
+    if (/^[1-9][0-9]{6}*$/.test(pin))
+        return true
+}
+const isValidPassword=(pw)=>{
+    if (/^[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(pw))
+    return true
+}
+const isValidTitle=(title)=>{
+    if(/^[a-zA-Z]+(([',. -][a-zA-Z0-9 ])?[a-zA-Z0-9]*)*$/.test(title))
+    return true
+}
+const isValidBody=(body)=>{
+    if(/^[a-zA-Z]+(([',. -][a-zA-Z0-9 ])?[a-zA-Z0-9]*)*$/.test(body))
+    return true
+}
+
+
+
+
+
+module.exports={isValidPincode,isValidPhone,isValidEmail,isValidName,isValid,isValidPassword,isValidTitle,isValidBody}
