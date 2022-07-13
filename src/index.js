07327e8a9fcd96d172e46a2f602130339de1b865
wishallmaurya <vm7740014@gmail.com>
2022-07-13 16:01:56 +0530

    Index updated

--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-mongoose.connect("mongodb+srv://wishall:vishal@atlascluster.p9u9uvd.mongodb.net/group59Database?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
+mongoose.connect("mongodb+srv://wishall:vishal@atlascluster.p9u9uvd.mongodb.net/group59Database?retryWrites=true&w=majority")
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
- 
-
-
 app.use('/', route);
-
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen( 3000, function () {
+    console.log('Express app running on port ' + (3000))
 });
