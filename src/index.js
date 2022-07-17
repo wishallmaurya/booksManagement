1dda62de5efbdbceb260047bc1801c4b2c7610e7
wishallmaurya <vm7740014@gmail.com>
2022-07-17 11:38:10 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const route = require('./routes/route');
 const { default: mongoose } = require('mongoose');
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect("mongodb+srv://wishall:vishal@atlascluster.p9u9uvd.mongodb.net/group59Database?retryWrites=true&w=majority")
+
+mongoose.connect("mongodb+srv://wishall:vishal@atlascluster.p9u9uvd.mongodb.net/booksDatabase?retryWrites=true&w=majority")
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
 app.use('/', route);
+
 app.listen( 3000, function () {
     console.log('Express app running on port ' + (3000))
 });
